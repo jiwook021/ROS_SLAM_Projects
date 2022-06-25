d0e2ec62fa74dea963daef11084190f7bbc62a12
jiwook021 <jiwook021@gmail.com>
2022-06-25 08:05:43 -0700

    efjefij

---
+++ src/ceres_tutorial.cpp
@@ -0,0 +1,14 @@
+#include <ceres/ceres.h>
+#include <ceres/covariance.h>
+#include <ceres/rotation.h>
+
+#include <chrono>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+
+int main(int argc, )
+{
+
+
+
+}
---
+++ src/hello.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sstream>
+#include <eigen3/Eigen/Dense>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "talker");
+    ros::NodeHandle n;
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1);
+
+    ros::Rate loop_rate(10);
+
+    // int count = 0;
+    while (ros::ok()) {
+        std_msgs::String msg;
+        // std::stringstream ss;
+        // ss << "hello world " << count;
+        // msg.data = ss.str();
+        msg.data = "jiwook";
+        // ROS_INFO("%s", msg.data.c_str());
+        chatter_pub.publish(msg);
+        // ros::spinOnce();
+        loop_rate.sleep();
+        // ++count;
+    }
+
+    return 0;
+}
