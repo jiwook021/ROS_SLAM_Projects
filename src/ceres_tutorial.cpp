30cb48148516be87b0c2fe7ad4ee866181ee638c
jiwook021 <jiwook021@gmail.com>
2022-07-03 22:47:29 -0700

    gr

--- src/ceres_tutorial.cpp
+++ src/ceres_tutorial.cpp
@@ -12,7 +12,13 @@ class CURVE_FITTING_COST{
 
     template<typename T>
     bool operator() (const T* const abc, T* residual) const {
-        residual[0] = T(_y) - abc[0] *T(_x)*T(_x)+abc[1]*T(_x)+abc[2];
+        //residual[0] = T(_y) - abc[0] *T(_x)*T(_x)+abc[1]*T(_x)+abc[2];
+        
+        residual[0] = T(_y) - (abc[0]*(T(_x)) - abc[1]*ceres::exp(T(_x)) + abc[2]* ceres::pow(T(_x),3) + abc[3]);
+        //   y_data.push_back(a*t - b*std::exp(t) + c*t*t*t + d);
+ 
+       // a*t-b*std::exp(t)+c*t*t*t+d
+
         return true;
     }
         CURVE_FITTING_COST(double x, double y){
@@ -29,37 +35,39 @@ int main()
 {
     
     // (a * x * x + b*x +c)
-printf("check1");
+    //(y = a*log(x) - b*exp(x) + c*x*x*x)
     double a = 1.5;
     double b = 4.5;
     double c= 6.6;
-    double abc[3];
+    double d= 1.0;
+    
+    double abc[4]={0,0,0,0};
 
     std::vector<double> x_data,y_data;
 
     int N = 100;
     
     for(int i = 0; i<N ;i++){
-        double t = i/N;
+        double t = (double) i/N;
         x_data.push_back(t);
-        y_data.push_back(a*t*t+b*t+c);
+        y_data.push_back(a*(t) - b*std::exp(t) + c*std::pow(t,3) + d);
     }
     
     /****complete generating data *****/
 
     /**solve problem with ceres***/
-    printf("check10");
     ceres::Problem problem;
-    printf("check11");
     for (int i = 0; i<N; i++){
         problem.AddResidualBlock(
-            new ceres::AutoDiffCostFunction<CURVE_FITTING_COST, 1, 3>(
+            new ceres::AutoDiffCostFunction<CURVE_FITTING_COST, 1, 4>(
                 new CURVE_FITTING_COST(x_data[i],y_data[i])),nullptr,abc
             );
         }
-printf("check2");
     ceres::Solver::Options options;
-    options.linear_solver_type = ceres::DENSE_QR;
+    //options.max_num_iterations = 300;
+    //options.linear_solver_type = ceres::DENSE_QR;
+    options.linear_solver_type = ceres::SPARSE_SCHUR;
+    
     //options.nonlinear_conjugate_gradient_type = ceres::LEVENBERG_MARQUARDT;
     options.minimizer_progress_to_stdout = true; 
 
@@ -70,5 +78,4 @@ printf("check2");
     for (auto &p : abc) std::cout << p <<std::endl;
 
     return 0;
-
 }
